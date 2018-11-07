4122016fee7890d061149f804a05a441bd23d789
Dave Wiard <dave.wiard@gmail.com>
2018-11-07 07:59:43 -0800

    Initial check-in of main script file.

---
+++ server.js
@@ -0,0 +1,40 @@
+//
+// Base setup
+//
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser
+// this will let us get the data from a POST request
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 3000;
+
+
+//
+// Routes for our API
+//
+
+var router = express.Router();
+
+// test route to make sure everything is working
+router.get('/', function(req, res) {
+    res.json({ message: 'Welcome to wiard-api!' });
+});
+
+// more routes will happen here
+
+// register our routes
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+
+//
+// Start the server
+//
+
+app.listen(port);
+console.log('The magic happens on port ' + port);
